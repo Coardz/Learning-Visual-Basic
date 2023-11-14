196dcbc0db0762cfc5d247dafb5c304838469baf
coardz <jericbanalz@gmail.com>
2023-11-14 16:36:00 +0800

    made a simple mood tracker with listbox

---
+++ Projects/mood tracker/mood tracker/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("mood tracker")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("mood tracker")> 
+<Assembly: AssemblyCopyright("Copyright ©  2023")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d7b9935c-034f-4e82-8623-77ef0fc8ffd1")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Projects/mood tracker/mood tracker/mood tracker.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "happy" Then
+            Label1.Text = "Good to hear :)"
+            ListBox1.Items.Add(TextBox1.Text)
+            MessageBox.Show("Saved", "Mood Tracker")
+        ElseIf TextBox1.Text = "sad" Then
+            Label1.Text = "Sad to hear :("
+            ListBox1.Items.Add(TextBox1.Text)
+            MessageBox.Show("Saved", "Mood Tracker")
+        Else
+            Label1.Text = "Ask a friend"
+            MessageBox.Show("Try again", "Mood Tracker")
+        End If
+
+    End Sub
+End Class
